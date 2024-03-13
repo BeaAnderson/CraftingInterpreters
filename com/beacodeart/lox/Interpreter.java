ba2a72a2f7800ff392705504012a4e1d6e3c0cf3
bea.anderson <bea.anderson@fdmgroup.com>
2024-03-13 15:25:19 +0000

    added ability to have global variables

--- com/beacodeart/lox/Interpreter.java
+++ com/beacodeart/lox/Interpreter.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 //our interpreter uses the visitor pattern
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
-	
+	private Environment environment = new Environment();	
 	//calls the entry point into the interpreter
 	void interpret (List<Stmt> statements){
 		try {
@@ -39,6 +39,17 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	public Void visitPrintStmt(Stmt.Print stmt){
 		Object value = evaluate(stmt.expression);
 		System.out.println(stringify(value));
+		return null;
+	}
+
+	@Override
+	public Void visitVarStmt(Stmt.Var stmt){
+		Object value = null;
+		if (stmt.initializer !=null){
+			value = evaluate(stmt.initializer);
+		}
+
+		environment.define(stmt.name.lexeme, value);
 		return null;
 	}
 	
@@ -122,6 +133,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 		}
 
 		return null;
+	}
+
+	@Override
+	public Object visitVariableExpr(Expr.Variable expr){
+		return environment.get(expr.name);
 	}
 
 	//helper methods
